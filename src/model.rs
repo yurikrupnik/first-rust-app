2a970dc3592ed390693fdf674bf431ccb898ae85
Yuri Krupnik <krupnik.yuri@gmail.com>
2023-06-25 20:07:50 +0300

    added cli, docker, main.rs

--- src/model.rs
+++ src/model.rs
@@ -11,6 +11,19 @@ pub struct User {
     pub role: String,
     pub age: i32,
     pub email: String,
+    // pub has_car: bool,
+    // pub password: String,
+}
+
+#[derive(Clone, TS, PartialEq, Eq, Deserialize, Serialize)]
+#[ts(export)]
+pub struct Book {
+    // pub _id: String,
+    // pub _id: Option<String>,
+    pub name: String,
+    pub year: i32,
+    pub writer: String,
+    // pub has_car: bool,
     // pub password: String,
 }
 
